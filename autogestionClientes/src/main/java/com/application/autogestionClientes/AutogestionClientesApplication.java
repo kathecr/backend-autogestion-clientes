31cf6550855187c051be8b974d8a508831a9cff6
kathecr <kcastelblancor@gmail.com>
2021-11-19 01:46:40 -0500

    Create spring project

---
+++ autogestionClientes/src/main/java/com/application/autogestionClientes/AutogestionClientesApplication.java
@@ -0,0 +1,13 @@
+package com.application.autogestionClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutogestionClientesApplication.class, args);
+	}
+
+}
